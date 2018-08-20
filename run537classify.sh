b8812928fc905bca19b235e5bd20060540744ae1
Eric Riebling <riebling@gmail.com>
2018-08-20 20:16:43 +0000

    sort RTTM by time

--- run537classify.sh
+++ run537classify.sh
@@ -6,9 +6,31 @@
 # takes one argument: path to an audio file
 # produces .rttm and .frame_prob.mat files in same folder
 
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in. /home/user/bin
+BASEDIR=`dirname $SCRIPT`
+
+DATA=$(readlink -f $1)
+OUT=`dirname $DATA`
+
+cd $BASEDIR
+
 # get the model if missing
 if [ ! -f model.pt ]; then
   wget http://speechkitchen.org/model.pt
 fi
 
 python predict.py $1
+
+filename=$(basename "$1")
+dirname=$(dirname "$1")
+extension="${filename##*.}"
+basename="${filename%.*}"
+output=$dirname/$basename.rttm
+
+#echo "basename is " $basename
+#exit
+
+mv $output /tmp
+sort -V -k3 /tmp/$basename.rttm > $OUT/$basename.rttm
